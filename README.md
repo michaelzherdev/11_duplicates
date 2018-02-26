1e91e48cd49dc2c441d9f21582f9e29e30b72d78
Mikhail <m.zherdev@tcombank.ru>
2018-02-26 11:55:14 +0300

    doppel

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+Program searched for duplicate files in passed directory
+
+# How to use
+Run from command line:
+```shell
+cd ${path_to_script}
+python duplicates.py <path-to-directory>
+```
+Example of output:
+
+```shell
+Duplicate files:
+/home/user/test/t/test2
+/home/user/test/test2
+```
+
+# How to run.
+
+For correct work of script Python 3.5 is required.
+
+Run on Linux/Windows:
+
+```bash
+$ python duplicates.py <path-to-directory>
+  # possibly requires call of python3 executive instead of just python
+```
+
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,23 @@
+import sys
+import os
+
 
 if __name__ == '__main__':
-    pass
+    working_dir = sys.argv[1]
+
+    files_dict = dict()
+    duplicate_files = []
+    for current_dir, subdirs, files in os.walk(working_dir):
+        for name in files:
+            path = os.path.join(current_dir, name)
+            size = os.stat(path).st_size
+
+            for key, value in files_dict.items():
+                if value == [name, size]:
+                    duplicate_files.append(path)
+                    duplicate_files.append(key)
+            files_dict[path] = [name, size]
+
+    print("Duplicate files:")
+    for duplicate in duplicate_files:
+        print(duplicate)
